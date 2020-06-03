469929ba2867ede1112cd661681b3080d814a594
James Hume <jameshume328@gmail.com>
2020-06-02 21:59:13 -0500

    implementing access control so guests can't add posts and users can only edit and delete their own posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <a href="/posts" class="btn btn-secondary">Go Back</a>
+    <br /><br />
     <h1>{{$post->title}}</h1>
     <div>
         {!!$post->body!!}
@@ -9,10 +10,14 @@
     <hr />
     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr />
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a>
-    
-    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
-        {{Form::hidden('_method', 'DELETE')}}
-        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-    {!!Form::close() !!}
+    @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a>
+            
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+                {{Form::hidden('_method', 'DELETE')}}
+                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+            {!!Form::close() !!}
+        @endif
+    @endif
 @endsection
